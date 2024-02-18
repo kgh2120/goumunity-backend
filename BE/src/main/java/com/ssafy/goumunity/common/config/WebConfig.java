eeed290779f40455c7405fc3a4e9713e3f224eee
kyoohyun <kgh2120@naver.com>
2024-02-19 00:18:02 +0900

    add: 요청 시간 측정 인터셉터 추가

---
+++ BE/src/main/java/com/ssafy/goumunity/common/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.ssafy.goumunity.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.ssafy.goumunity.common.interceptor.PerformanceInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new PerformanceInterceptor());
+	}
+}
---
+++ BE/src/main/java/com/ssafy/goumunity/common/interceptor/PerformanceInterceptor.java
@@ -0,0 +1,31 @@
+package com.ssafy.goumunity.common.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PerformanceInterceptor implements HandlerInterceptor {
+
+	private ThreadLocal<Long> perfTime = new ThreadLocal<>();
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
+		Exception {
+		perfTime.set(System.currentTimeMillis());
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+		ModelAndView modelAndView) throws Exception {
+		long last = System.currentTimeMillis();
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+		Long startTime = perfTime.get();
+		log.info("[{}{}]  cost {}ms", request.getMethod(), request.getRequestURI(), last-startTime);
+		perfTime.remove();
+	}
+}
